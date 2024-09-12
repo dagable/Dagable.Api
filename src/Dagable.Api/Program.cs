4881b5ef901e74d87f64d0befb61c3844e73e1b0
John <22085211+jwmxyz@users.noreply.github.com>
2024-09-12 21:10:25 +0100

    added db migrations

--- src/Dagable.Api/Program.cs
+++ src/Dagable.Api/Program.cs
@@ -1,10 +1,20 @@
 using Dagable.Api.Startup;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
+using Dagable.DataAccess.Migrations;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.RegisterApplicationServices(builder);
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DagableDbContext>();
+    db.Database.Migrate();
+}
 
 app.ConfigureMiddleware();
 app.RegisterEndpoints();
